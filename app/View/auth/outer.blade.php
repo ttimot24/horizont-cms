e69e53b595d13096e65bb2e205eebe4d3dee59b4
Timot Tarjani <ttimotster@gmail.com>
2026-05-19 17:06:06 +0200

    refact: remove jumbotron from login screen

--- app/View/auth/outer.blade.php
+++ app/View/auth/outer.blade.php
@@ -4,15 +4,15 @@
 
 @section('content')
 
-<div class='jumbotron'>
+<div class='bg-dark p-5 text-white mb-5'>
     <div class='container my-5'>
         <div class="row">
             <div class="col-md-2 col-xs-12 text-center">
                 <img src='{{ $admin_logo }}' width="150" alt="">
             </div>
             <div class="col-md-4 col-xs-12 text-center">
-                <h1 class="pt-3">{{ $app_name }}</h1>
-                <p><q><i>Closer to the web</i></q></p>
+                <h1 class="pt-3 hero-title">{{ $app_name }}</h1>
+                <h3><q><i>Closer to the web</i></q></h3>
             </div>
             <div class="col-md-6">
             </div>
